ce13fc1ed1aad0f59731debf87648dd34daec3bf
Piyush Baraskar <piyushbaraskar4@gmail.com>
2025-09-25 22:20:38 +0530

    Update STL.cpp

--- basics/STL.cpp
+++ basics/STL.cpp
@@ -1,139 +1,204 @@
-/*
-An unordered set in STL is a container that stores unique elements in no particular order. Every operation on an unordered set takes O(1) complexity in the average case and takes O(n) in the worst case.
-
-Syntax:
-unordered_set<object_type> variable_name;
-*/
-
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-//   unordered_set < int > s;
-//   for (int i = 1; i <= 10; i++) {
-//     s.insert(i);
-//   }
-
-//   cout << "Elements present in the unordered set: ";
-//   for (auto it = s.begin(); it != s.end(); it++) {
-//     cout << * it << " ";
-//   }
-//   cout << endl;
-//   int n = 2;
-//   if (s.find(2) != s.end())
-//     cout << n << " is present in unordered set" << endl;
-
-//   s.erase(s.begin());
-//   cout << "Elements after deleting the first element: ";
-//   for (auto it = s.begin(); it != s.end(); it++) {
-//     cout << * it << " ";
-//   }
-//   cout << endl;
-
-//   cout << "The size of the unordered set is: " << s.size() << endl;
-
-//   if (s.empty() == false)
-//     cout << "The unordered set is not empty " << endl;
-//   else
-//     cout << "The unordered set is empty" << endl;
-//   s.clear();
-//   cout << "Size of the unordered set after clearing all the elements: " << s.size();
-// }
-
-/*------------------------------------------------------------------------------------------------------------------------------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
 
 /*
-What is a Vector?
-Vectors in STL are basically dynamic arrays that have the ability to change size whenever elements are added or deleted from them. Vector elements can be easily accessed and traversed using iterators. A vector stores elements in contiguous memory locations.
+----------------------------------------------------------------------------------------------------
+    VOID FUNCTION
+----------------------------------------------------------------------------------------------------
+    - A void function does not return a value.
+    - Useful when only performing actions, not producing results. */
 
-Syntax:
-vector<object_type> variable_name;
+//  void print() {
+//         cout << "raj";
+//         }
 
-*/
+//         int sum(int a, int b) {
+//         return a + b;
+//         }
 
-// #include<bits/stdc++.h>
-// using namespace std;
+//         int main() {
+//         int c = sum(2, 3);
+//         cout << c << endl;
+//         return 0;
+//         }
 
-// int main() {
-//   vector < int > v;
+       
+/*----------------------------------------------------------------------------------------------------*/
 
-//   for (int i = 0; i < 10; i++) {
-//     v.push_back(i); //inserting elements in the vector
-//   }
-
-//   cout << "the elements in the vector: ";
-//   for (auto it = v.begin(); it != v.end(); it++)
-//     cout << * it << " ";
-
-//   cout << "\nThe front element of the vector: " << v.front();
-//   cout << "\nThe last element of the vector: " << v.back();
-//   cout << "\nThe size of the vector: " << v.size();
-//   cout << "\nDeleting element from the end: " << v[v.size() - 1];
-//   v.pop_back();
-
-//   cout << "\nPrinting the vector after removing the last element:" << endl;
-//   for (int i = 0; i < v.size(); i++)
-//     cout << v[i] << " ";
-
-//   cout << "\nInserting 5 at the beginning:" << endl;
-//   v.insert(v.begin(), 5);
-//   cout << "The first element is: " << v[0] << endl;
-//   cout << "Erasing the first element" << endl;
-//   v.erase(v.begin());
-//   cout << "Now the first element is: " << v[0] << endl;
-
-//   if (v.empty())
-//     cout << "\nvector is empty";
-//   else
-//     cout << "\nvector is not empty" << endl;
-
-//   v.clear();
-//   cout << "Size of the vector after clearing the vector: " << v.size();
-// }
-
-/*------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /*
-What is a set?
-A set in STL is a container that stores unique elements in a particular order. Every operation on a set takes O(1) complexity in the average case and takes O(n) in the worst case.
+----------------------------------------------------------------------------------------------------
+    PAIR IN STL
+----------------------------------------------------------------------------------------------------
+    - A pair stores 2 values (possibly of different types) together.
+    - Elements are accessed as .first and .second. */
 
-Syntax:
-set<object_type> variable_name;
+ 
+        // void testPair() {
+        //     pair<int,int> p = {1, 3};
+        //     cout << p.first << " " << p.second;
+
+        //     pair<int,pair<int,int>> p2 = {1,{2,3}};
+        //     cout << p2.first << " " << p2.second.second << " " << p2.second.first;
+
+        //     pair<int,int> arr[] = {{1,2},{2,5},{5,1}};
+        //     cout << arr[1].second;
+        // }
+
+        // int main() {
+        //     testPair();
+        //     return 0;
+        // }
+/*----------------------------------------------------------------------------------------------------*/
+
+/*
+----------------------------------------------------------------------------------------------------
+    UNORDERED SET IN STL
+----------------------------------------------------------------------------------------------------
+    - Stores unique elements in *no particular order*.
+    - Average case time complexity: O(1)
+    - Worst case time complexity: O(n)
+
+    Syntax:
+        unordered_set<object_type> variable_name; */
+
+    
+        // int main() {
+        //     unordered_set<int> s;
+        //     for (int i = 1; i <= 10; i++) {
+        //         s.insert(i);
+        //     }
+
+        //     cout << "Elements present in the unordered set: ";
+        //     for (auto it = s.begin(); it != s.end(); it++) {
+        //         cout << *it << " ";
+        //     }
+        //     cout << endl;
+
+        //     int n = 2;
+        //     if (s.find(2) != s.end())
+        //         cout << n << " is present in unordered set" << endl;
+
+        //     s.erase(s.begin());
+        //     cout << "Elements after deleting the first element: ";
+        //     for (auto it = s.begin(); it != s.end(); it++) {
+        //         cout << *it << " ";
+        //     }
+        //     cout << endl;
+
+        //     cout << "The size of the unordered set is: " << s.size() << endl;
+
+        //     if (!s.empty())
+        //         cout << "The unordered set is not empty " << endl;
+        //     else
+        //         cout << "The unordered set is empty" << endl;
+
+        //     s.clear();
+        //     cout << "Size of the unordered set after clearing: " << s.size();
+        // }
+/*----------------------------------------------------------------------------------------------------*/
+
+/*
+----------------------------------------------------------------------------------------------------
+    VECTOR IN STL
+----------------------------------------------------------------------------------------------------
+    - Dynamic array: can grow/shrink in size.
+    - Elements are stored in contiguous memory locations.
+    - Can be traversed easily with iterators.
+
+    Syntax:
+        vector<object_type> variable_name; */
+
+   
+        // int main() {
+        //     vector<int> v;
+        //     for (int i = 0; i < 10; i++) {
+        //         v.push_back(i);
+        //     }
+
+        //     cout << "The elements in the vector: ";
+        //     for (auto it = v.begin(); it != v.end(); it++)
+        //         cout << *it << " ";
+
+        //     cout << "\nThe front element: " << v.front();
+        //     cout << "\nThe last element: " << v.back();
+        //     cout << "\nThe size of the vector: " << v.size();
+
+        //     cout << "\nDeleting element from the end: " << v[v.size()-1];
+        //     v.pop_back();
+
+        //     cout << "\nAfter removing the last element: ";
+        //     for (int i = 0; i < v.size(); i++)
+        //         cout << v[i] << " ";
+
+        //     cout << "\nInserting 5 at the beginning..." << endl;
+        //     v.insert(v.begin(), 5);
+        //     cout << "The first element is: " << v[0] << endl;
+
+        //     cout << "Erasing the first element..." << endl;
+        //     v.erase(v.begin());
+        //     cout << "Now the first element is: " << v[0] << endl;
+
+        //     if (v.empty())
+        //         cout << "\nVector is empty";
+        //     else
+        //         cout << "\nVector is not empty" << endl;
+
+        //     v.clear();
+        //     cout << "Size of the vector after clearing: " << v.size();
+        // }
+/*----------------------------------------------------------------------------------------------------*/
+
+/*
+----------------------------------------------------------------------------------------------------
+    SET IN STL
+----------------------------------------------------------------------------------------------------
+    - Stores unique elements in *sorted order*.
+    - Average case time complexity: O(log n)
+    - Worst case time complexity: O(n)
+
+    Syntax:
+        set<object_type> variable_name; */
+
+    
+        // int main() {
+        //     set<int> s;
+        //     for (int i = 1; i <= 10; i++) {
+        //         s.insert(i);
+        //     }
+
+        //     cout << "Elements present in the set: ";
+        //     for (auto it = s.begin(); it != s.end(); it++) {
+        //         cout << *it << " ";
+        //     }
+        //     cout << endl;
+
+        //     int n = 2;
+        //     if (s.find(2) != s.end())
+        //         cout << n << " is present in set" << endl;
+
+        //     s.erase(s.begin());
+        //     cout << "Elements after deleting the first element: ";
+        //     for (auto it = s.begin(); it != s.end(); it++) {
+        //         cout << *it << " ";
+        //     }
+        //     cout << endl;
+
+        //     cout << "The size of the set is: " << s.size() << endl;
+
+        //     if (!s.empty())
+        //         cout << "The set is not empty " << endl;
+        //     else
+        //         cout << "The set is empty" << endl;
+
+        //     s.clear();
+        //     cout << "Size of the set after clearing: " << s.size();
+        // }
+/*----------------------------------------------------------------------------------------------------*/
+
+/*
+====================================================================================================================
+                                               THE END 
+====================================================================================================================
 */
 
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-//   set < int > s;
-//   for (int i = 1; i <= 10; i++) {
-//     s.insert(i);
-//   }
-
-//   cout << "Elements present in the set: ";
-//   for (auto it = s.begin(); it != s.end(); it++) {
-//     cout << * it << " ";
-//   }
-//   cout << endl;
-//   int n = 2;
-//   if (s.find(2) != s.end())
-//     cout << n << " is present in set" << endl;
-
-//   s.erase(s.begin());
-//   cout << "Elements after deleting the first element: ";
-//   for (auto it = s.begin(); it != s.end(); it++) {
-//     cout << * it << " ";
-//   }
-//   cout << endl;
-
-//   cout << "The size of the set is: " << s.size() << endl;
-
-//   if (s.empty() == false)
-//     cout << "The set is not empty " << endl;
-//   else
-//     cout << "The set is empty" << endl;
-//   s.clear();
-//   cout << "Size of the set after clearing all the elements: " << s.size();
-// }
-
-/*------------------------------------------------------------------------------------------------------------------------------------------------*/
